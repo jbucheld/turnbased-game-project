e4ea6a329c649220bee20bb700b0032f9593f580
jBucheld <jakub.bucheld@gmail.com>
2026-06-02 11:13:22 +0200

    added unit rotation on move

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -11,11 +11,13 @@ public class Unit : MonoBehaviour
 {
     private PlayerInput _player;
     private TacticalViewController _controller;
+    [SerializeField] private Animator _animator;
 
     private float stoppingDistance = 0.05f;
     
     private Vector3 targetPosition;
     [SerializeField] private float unitMoveSpeed = 4f;
+    [SerializeField] private float unitRotationSpeed = 12f;
     
 
     private void Start()
@@ -36,8 +38,15 @@ public class Unit : MonoBehaviour
         // apply simple move mechanism
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
+            _animator.SetBool("IsWalking", true);
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            
             transform.position +=  moveDirection * (Time.deltaTime * unitMoveSpeed);
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * unitRotationSpeed);
+        }
+        else
+        {
+            _animator.SetBool("IsWalking", false);
         }
     }
 
